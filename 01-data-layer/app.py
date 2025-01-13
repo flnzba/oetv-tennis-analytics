336e5c807e1c024f389e2b910fc1c987c1709568
flnzba <florian.zeba@proton.me>
2025-01-13 15:50:24 +0100

    Refactor error handling and improve data processing in app.py; update error log format and enhance database model in db-load.py

--- 01-data-layer/app.py
+++ 01-data-layer/app.py
@@ -1,4 +1,5 @@
 from curl_cffi import requests  # type: ignore
+import json
 import os
 
 # no additional crawling library needed
@@ -7,8 +8,8 @@ import os
 
 def handle_error(status_code):
     print("Error: ", status_code)
-    with open("../test/error-last-call.txt", "w") as file:
-        file.write(f"Error: {status_code}")
+    with open("../test/error-last-call.txt", "a") as file:
+        file.write(f"Error: {status_code} \n")
 
 
 url = "https://www.oetv.at/?oetvappapi=1&apikey=QWXWLwYAtSFvJGmyFtEMlypWS6fH71wk&method=nu-ranking&firstResult=0&ageRange=&subtype=general&region=&gender=male&type=oetv&itnFrom=&rankFrom=&search="
@@ -34,17 +35,52 @@ referer = "https://www.oetv.at/rangliste/"
 # last error is saved in ./test/error-last-call.txt
 # when error occurs last response is preserved in the output folder and error is written to a file
 
-try:
-    r = requests.get(url, headers=headers, referer=referer, impersonate="chrome")
-    c = requests.Session()
 
-    if r.status_code == 200:
-        with open("../test/oetv.json", "w") as file:
-            file.write(r.text)
-    else:
-        handle_error(r.status_code)
-except Exception as e:
-    handle_error(e)
+def main():
+    try:
+        r = requests.get(url, headers=headers, referer=referer, impersonate="chrome")
+        # c = requests.Session()
+        # print(r.text)
+
+        if r.status_code == 200:
+            with open("../test/oetv.json", "w") as file:
+                file.write(r.text)
+
+            # Parse the JSON response
+            rankings_data = json.loads(r.text)
+            # iterate into the second object to get the data object
+            d = list(rankings_data.values())[1]
+            # print(d)
+            # iterate into the "ranking" object to get the players
+            rankings_data = list(d.values())[3]
+
+            # Save the JSON response to a file
+            with open("../test/oetv-rankings.json", "w") as file:
+                rankings_data_write = str(rankings_data).replace("'", '"')
+                rankings_data_write = rankings_data_write.replace("False", '"False"')
+                file.write(str(rankings_data_write))
+
+        else:
+            handle_error(r.status_code)
+
+    except Exception as e:
+        handle_error(e)
+
+    return rankings_data
+
+
+def parse():
+    try:
+        # load data from json so api call to oetv is not needed every time
+        with open("../test/oetv-rankings.json", "r") as file:
+            data = json.load(file)
+
+        for i in data:
+            print(i)
+
+    except Exception as e:
+        handle_error(e)
+
 
 if __name__ == "__main__":
-    pass
+    parse()
--- 02-business-layer/db-load.py
+++ 02-business-layer/db-load.py
@@ -1,15 +1,20 @@
 import sqlite3
-import pydantic
+from pydantic import BaseModel as basem  # type: ignore
 
 
-def save_to_db(data):
-    conn = sqlite3.connect("data.db")
-    c = conn.cursor()
-    c.execute("""CREATE TABLE IF NOT EXISTS players (name TEXT, rank TEXT)""")
-    for item in data:
-        c.execute(
-            """INSERT INTO players (name, rank) VALUES (?, ?)""",
-            (item["name"], item["rank"]),
-        )
-    conn.commit()
-    conn.close()
+class Player(basem):
+    name: str
+    rank: str
+
+
+# def save_to_db(data):
+#     conn = sqlite3.connect("data.db")
+#     c = conn.cursor()
+#     c.execute("""CREATE TABLE IF NOT EXISTS players (name TEXT, rank TEXT)""")
+#     for item in data:
+#         c.execute(
+#             """INSERT INTO players (name, rank) VALUES (?, ?)""",
+#             (item["name"], item["rank"]),
+#         )
+#     conn.commit()
+#     conn.close()
